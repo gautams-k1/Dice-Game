f5f6c40bc8ca47e0bcc20e48df3c838bc3994167
gautams-k1 <bannabanna808@gmail.com>
2023-11-11 17:49:34 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dice-Game/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
+});
+
+
